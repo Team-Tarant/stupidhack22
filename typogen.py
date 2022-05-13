65d8235f68fd1342fcccb7f057b3b104cab20a10
mindler-sasu <sasu@hey.com>
2022-05-13 18:46:53 +0300

    inital stolen typogen

---
+++ typogen.py
@@ -0,0 +1,35 @@
+import random
+
+typos = {'a': "qwsz",   'b': "vghn",   'c': "xdfv",
+         'd': "serfcx", 'e': "wsdfr",  'f': "dcvgtr",
+         'g': "fvbhyt", 'h': "gbnjuy", 'i': "ujko",
+         'j': "hnmkıu", 'k': "jmloi",  'l': "mkop",
+         'm': "njkl",   'n': "bhjm",   'o': "iklp",
+         'p': "ol",     'q': "wa",     'r': "edft",
+         's': "wazxde", 't': "rfgy",   'u': "yhji",
+         'v': "cfgb",   'w': "qase",   'x': "zsdc",
+         'y': "tghu",   'z': "asx",    ' ': " "}
+
+FAILRATE = 0.175
+
+while(True):
+    msg = input("\nMsg : ")
+    msg = msg.lower() #convert msg to lowercase
+
+    res = "" #result string
+
+    # iterate over all characters and change them randomly
+    for c in msg:
+        if(c in typos):
+            deter = random.uniform(0, 1)
+                
+            if (deter <= FAILRATE): #there is a typo now
+                res += typos[c][random.choice(range(0, len(typos[c])))]
+                    
+            else:
+                res = res + c
+        else:
+            res = res + c
+
+    #report typo string
+    print ("Typod: " + res + "\n\n")    

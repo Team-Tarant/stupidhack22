603ee4fdd7a62efa511f7ad5c9e934a35eb3b043
mindler-sasu <sasu@hey.com>
2022-05-14 15:09:42 +0300

    ramping blurse

--- transformation-engine/src/typogen.ts
+++ transformation-engine/src/typogen.ts
@@ -1,13 +1,16 @@
 import sample from 'lodash.samplesize'
 import { typos } from './utils/shit'
 
-const CHANCE_OF_TYPO = 0.1
+const BASE_CHANCE_OF_TYPO = 0.02
+let blursed = 1
 
 export const generateTypo = (inputChar: string) => {
   if (!(inputChar in typos)) return inputChar
 
   const buzzista = Math.random()
+  const CHANCE_OF_TYPO = BASE_CHANCE_OF_TYPO * blursed
   if (buzzista <= 0.1 * CHANCE_OF_TYPO) {
+    blursed = 1
     return sample(
       [
         ' vittu ',
@@ -23,11 +26,13 @@ export const generateTypo = (inputChar: string) => {
     ).pop()
   }
   if (buzzista <= CHANCE_OF_TYPO) {
+    blursed = 1
     const returnableTypo = sample(
       [...new Set([...typos[inputChar]])],
       sample([1, 2], 1).pop()
     )
     return returnableTypo.join('')
   }
+  blursed = blursed + 1
   return inputChar
 }

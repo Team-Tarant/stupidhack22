9e0943b7bfcab409b08cd10a4fca840f4af3b889
mindler-sasu <sasu@hey.com>
2022-05-14 14:26:37 +0300

    betterrefegsex

--- transformation-engine/src/datamine.ts
+++ transformation-engine/src/datamine.ts
@@ -56,9 +56,7 @@ export const datamine = (
     })
   }
 
-  const cc = stringistä.match(
-    /^(?:4[0-9]{12}(?:[0-9]{3})?|[25][1-7][0-9]{14}|6(?:011|5[0-9][0-9])[0-9]{12}|3[47][0-9]{13}|3(?:0[0-5]|[68][0-9])[0-9]{11}|(?:2131|1800|35\d{3})\d{11})$/g
-  )
+  const cc = stringistä.match(/((?:\d\d\d\d ?){4})/g)
   if (cc && cc[0].length === 16) {
     goldista.push({ type: 'creditCard', value: cc[0] })
   }
